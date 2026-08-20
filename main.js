4f8ddf2001afd798034a9b4797fcb16f0aa2cc58
Miguelescola062 <miguel.cardoso.santos@escola.pr.gov.br>
2026-08-20 09:05:52 -0300

    .

--- main.js
+++ main.js
@@ -76,9 +76,18 @@ const perguntas = [
     },
 ]
 
+
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = ""
+
 function mostraPerguntas() {
-    
+
+if (atual>=perguntas.length) {
+    mostraResultado()
+    return
+    }
+    perguntaAtual=perguntas[atual]
+    caixaPerguntas.textContent=perguntaAtual
 }
