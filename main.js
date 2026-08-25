6df28bb11d8a63eabb4dff774b21bb6648df3cfc
Miguelescola062 <miguel.cardoso.santos@escola.pr.gov.br>
2026-08-25 12:20:15 -0300

    .

--- main.js
+++ main.js
@@ -120,9 +120,6 @@ function mostraResultado(){
 caixaPerguntas.textContent="Em 2049..."
 textoResultado.textContent= historiaFinal
 caixaAlternativas.textContent="";
-caixaResultado.textContent= historiaFinal
-caixaAlternativas.textContent="";
-
 }
 
 
