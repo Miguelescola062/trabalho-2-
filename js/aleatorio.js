5f0bf083312bf027d961180deca1fb634b954064
Miguelescola062 <miguel.cardoso.santos@escola.pr.gov.br>
2026-09-17 09:20:27 -0300

    .

---
+++ js/aleatorio.js
@@ -0,0 +1,12 @@
+function aleatorio(lista){
+
+const posicao = Math.floor(Math.random()*lista.length)
+console.log(posicao)
+return(lista[posicao])
+
+
+
+}
+ export function aleatorio(lista){
+    const posicao = Math.floor(Math.random()=*lista.length)
+ }
--- js/main.js
+++ js/main.js
@@ -122,17 +122,6 @@ caixaAlternativas.textContent="";
 }
 
 
-function aleatorio(lista){
-
-const posicao = Math.floor(Math.random()*lista.length)
-console.log(posicao)
-return(lista[posicao])
-
-
-
-}
-
-
 
 
 mostraPergunta()
