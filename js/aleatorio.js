551037cd7802b2cf42793ae583aabc2111f00d0e
Miguelescola062 <miguel.cardoso.santos@escola.pr.gov.br>
2026-09-22 12:09:23 -0300

    l

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-function aleatorio(lista){
+ export function aleatorio(lista){
 
 const posicao = Math.floor(Math.random()*lista.length)
 console.log(posicao)
@@ -7,6 +7,3 @@ return(lista[posicao])
 
 
 }
- export function aleatorio(lista){
-    const posicao = Math.floor(Math.random()=*lista.length)
- }
